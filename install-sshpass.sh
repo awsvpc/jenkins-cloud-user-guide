4b4acac07a559aee164ae74ad5051ac7796f3d7d
awsvpc <88736238+awsvpc@users.noreply.github.com>
2024-04-15 22:01:58 -0400

    Create install-sshpass.sh

---
+++ install-sshpass.sh
@@ -0,0 +1,5 @@
+sudo yum update -y amazon-linux-extras
+sudo yum install -y amazon-linux-extras
+sudo amazon-linux-extras install epel -y
+sudo yum-config-manager --enable epel
+sudo yum install sshpass
